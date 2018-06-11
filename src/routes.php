ef0cf22bf38cf264cabbba900509dcb5832633b3
Fasasi Faiz <faizfasasi@gmail.com>
2018-06-11 16:11:13 +0100

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -5,6 +5,15 @@ use Slim\Http\Response;
 
 
 // Root
+
+/*$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});*/
+
 $app->get('/', 'HomeController:index');
 
 
